24567a5787399732d67134cfd2ca8263624906a4
Daniel Merritt <daniel.mafou@gmail.com>
2023-05-30 15:24:19 -0400

    bit.io inclusion

--- main_app/views.py
+++ main_app/views.py
@@ -1,28 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import HttpResponse
 
-class Cat:  # Note that parens are optional if not inheriting from another class
+class Monster:  # Note that parens are optional if not inheriting from another class
   def __init__(self, name, breed, description, age):
     self.name = name
     self.breed = breed
     self.description = description
     self.age = age
 
-cats = [
-  Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-  Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+monsters = [
+  Monster('Lolo', 'eldritch horror', 'Kinda rude.', 3),
+  Monster('Sachi', 'werewolf', 'Looks like a turtle.', 0),
+  Monster('Fancy', 'cyclops', 'Happy fluff ball.', 4),
+  Monster('Bonk', 'witch', 'Meows loudly.', 6)
 ]
 
 def home(request):
-  return HttpResponse('<h1>Hello, I am Mike Wazowski</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def monster_index(request):
   print('hello')
-  return render(request, 'monsters/index.html', {'cats': cats})
+  return render(request, 'monsters/index.html', {'monsters': monsters})
