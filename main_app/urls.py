7bc254458ea460118e16857a67c88947d0cfd9e8
Daniel Merritt <daniel.mafou@gmail.com>
2023-05-31 11:30:09 -0400

    detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('monsters/', views.monster_index, name='monster-index')
+    path('monsters/', views.monster_index, name='monster-index'),
+    path("monsters/<int:monster_id>/", views.monster_detail, name="monster-detail")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,7 @@
 from django.shortcuts import render
+from .models import Monster
 
 # Create your views here.
-
-class Monster:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-monsters = [
-  Monster('Lolo', 'eldritch horror', 'Kinda rude.', 3),
-  Monster('Sachi', 'werewolf', 'Looks like a turtle.', 0),
-  Monster('Fancy', 'cyclops', 'Happy fluff ball.', 4),
-  Monster('Bonk', 'witch', 'Meows loudly.', 6)
-]
 
 def home(request):
   return render(request, 'home.html')
@@ -23,5 +10,9 @@ def about(request):
   return render(request, 'about.html')
 
 def monster_index(request):
-  print('hello')
+  monsters = Monster.objects.all()
   return render(request, 'monsters/index.html', {'monsters': monsters})
+
+def monster_detail(request, monster_id):
+  monster = Monster.objects.get(id=monster_id)
+  return render(request, 'monsters/detail.html', { 'monster': monster})
